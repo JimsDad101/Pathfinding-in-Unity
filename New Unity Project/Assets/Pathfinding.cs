794bc03b4f3d86ff1b7078d89d9cf6af3e86ec02
JimsDad101 <harrywarland12345@gmail.com>
2020-11-25 10:23:31 +0000

    First Upload.
    Simple Pathfinding just moving around

---
+++ New Unity Project/Assets/Pathfinding.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Pathfinding : MonoBehaviour
+{
+public Transform[] points;
+
+private NavMeshAgent nav;
+private int destinationPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!nav.pathPending && nav.remainingDistance < 0.5f)
+        {
+            GoToNextPoint();
+        }
+    }
+    void GoToNextPoint()
+    {
+        if (points.Length == 0)
+        {
+            return;
+        }
+
+        nav.destination = points[destinationPoint].position;
+        destinationPoint = (destinationPoint + 1) % points.Length;
+    }
+}
