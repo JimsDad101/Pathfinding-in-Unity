fe354b0e642abe4ca3e27b5969dd17944daeabc5
JimsDad101 <harrywarland12345@gmail.com>
2020-12-02 12:14:58 +0000

    asd#

---
+++ New Unity Project/Assets/Score.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "" + scoreText;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + scoreText;
+    }
+
+     void OnCollisionEnter (Collision col) 
+        {
+          if (col.gameObject.CompareTag("pellet"))
+            {
+              scoreText = scoreText=+ 1;
+            }
+        }
+}
+
